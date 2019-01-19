ba1c2ac44f013d911613068d914d559b8817dbb3
Dario Furlan <dario.furlan.51@gmail.com>
2019-01-19 10:25:56 +0000

    issues with the border of the canvas

--- src/supervisor.js
+++ src/supervisor.js
@@ -7,9 +7,18 @@ const paint_box = $('#paint-box');
 const canvas = document.getElementById('canvas');
 const edge_size_input = document.getElementById('edge');
 
+edge_size_input.onchange = (e) => {
+    let v = edge_size_input.value;
+    if (Math.log2(v) % 1 === 0) {
+        console.log(v);
+        n_squares = v;
+        nn.update();
+    }
+};
 
 /* -------------------------------CONSTANTS------------------------------- */
 const DEFAULT_N_SQUARES = 16;
+
 
 
 /* -------------------------------VARIABLES------------------------------- */
@@ -17,9 +26,6 @@ let bound_canvas;
 let n_squares = DEFAULT_N_SQUARES;
 const nn = new NN(n_squares);
 const drawer = new Drawer(canvas, nn.out);
-nn.on('update', () => {
-    drawer.drawGrid();
-});
 
 
 
@@ -31,10 +37,39 @@ function init() {
         let y = e.pageY - bound_canvas.top;
 
         let pos = drawer.getPosition(x, y);
+
+        let ns = Math.sqrt(nn.out.length);
+
         nn.out[pos] = 1;
+        console.log(ns);
+        if (pos > (ns-1)) {
+            if (nn.out[pos-ns] === 0) {
+                nn.out[pos - ns] = .5;
+            }
+        }
+        if (pos < nn.out.length-(ns-1)) {
+            if (nn.out[pos+ns] === 0) {
+                nn.out[pos+ns] = .5;
+            }
+        }
+        if (pos%ns!==0) {
+            if (nn.out[pos-1] === 0) {
+                nn.out[pos-1] = .5;
+            }
+        }
+
+        if ((pos+1)%ns!==0) {
+            if (nn.out[pos+1] === 0) {
+                nn.out[pos+1] = .5;
+            }
+        }
+
+
         nn.emit('update');
     }
-
+    nn.on('update', () => {
+        drawer.drawGrid();
+    });
     window.ontouchstart = (e) => {
         console.log("touch started");
         draw(e);
@@ -65,18 +100,20 @@ function init() {
         }
         draw(e);
     };
-    canvas.addEventListener('mousedown', function (e) {
-        console.log("down");
+    canvas.onmousedown = (e) => {
         drawer.isDrawing = true;
-    });
-    canvas.addEventListener('mouseup', function (e) {
-        console.log("up");
+    };
+    canvas.onmouseup = (e) => {
         drawer.isDrawing = false;
-    });
+    };
+    canvas.onmouseleave = (e) => {
+        drawer.isDrawing = false;
+    };
 }
 
 function reduce() {
-    if (nn.out.length / 4 >= 64)
+    const MIN_LIMIT = 8;
+    if (nn.out.length / 4 >= MIN_LIMIT*MIN_LIMIT)
         nn.reduce();
 }
 
